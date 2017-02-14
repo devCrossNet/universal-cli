bb37db43594e184b3d85ee981a164e084a7635ff
Jason Jean <jasonjean1993@gmail.com>
2017-02-14 02:57:34 -0500

    fix(tests): use app main

--- tests/e2e/tests/build/aot.ts
+++ tests/e2e/tests/build/aot.ts
@@ -1,9 +1,9 @@
 import { ng } from '../../utils/process';
 import { expectFileToMatch } from '../../utils/fs';
-import { getClientDist } from '../../utils/utils';
+import { getClientDist, getAppMain } from '../../utils/utils';
 
 export default function () {
   return ng('build', '--aot')
-    .then(() => expectFileToMatch(`${getClientDist()}client.bundle.js`,
+    .then(() => expectFileToMatch(`${getClientDist()}${getAppMain()}.bundle.js`,
       /bootstrapModuleFactory.*\/\* BrowserAppModuleNgFactory \*\//));
 }
